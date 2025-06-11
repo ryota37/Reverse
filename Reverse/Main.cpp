00b201557cb3fc04df775e3bbd7fba02df5b4197
ryota37 <ryota.nakahama@access-company.com>
2025-06-11 18:56:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Reverse/Main.cpp
@@ -0,0 +1,26 @@
+﻿# include <Siv3D.hpp>
+
+void DrawGrid(const Grid<int32>& grid)
+{
+	for (int32 y = 0; y < grid.height(); ++y)
+	{
+		for (int32 x = 0; x < grid.width(); ++x)
+		{
+			const RectF rect{ (x * 100), (y * 100), 100 };
+			rect.stretched(-1).draw();
+		}
+	}
+}
+
+void Main()
+{
+	Window::Resize(800, 800);
+	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+
+	Grid<int32> grid(8, 8);
+
+	while (System::Update())
+	{
+		DrawGrid(grid);
+	}
+}
