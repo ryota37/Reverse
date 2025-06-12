ca5b11d36175b120d2ac048dd3aa6ea63fe679a1
ryota37 <ryota.nakahama@access-company.com>
2025-06-12 19:20:32 +0900

    Impl: Draw stones

--- Reverse/Main.cpp
+++ Reverse/Main.cpp
@@ -7,7 +7,28 @@ void DrawGrid(const Grid<int32>& grid)
 		for (int32 x = 0; x < grid.width(); ++x)
 		{
 			const RectF rect{ (x * 100), (y * 100), 100 };
-			rect.stretched(-1).draw();
+			rect.stretched(-1).draw(Palette::Green);
+		}
+	}
+}
+
+enum class StoneColor
+{
+	None,
+	Black,
+	White
+};
+
+void DrawStone(const Grid<StoneColor>& boardState)
+{
+	for (int y = 0; y < boardState.height(); ++y)
+	{
+		for (int x = 0; x < boardState.width(); ++x)
+		{
+			Circle stone{50 + 100*x, 50 + 100 * y, 25};
+			if (boardState[y][x] == StoneColor::Black) stone.draw(Palette::Black);
+			if (boardState[y][x] == StoneColor::White) stone.draw(Palette::White);
+			if (boardState[y][x] == StoneColor::None) stone.draw(Palette::Red); // Debug
 		}
 	}
 }
@@ -15,12 +36,13 @@ void DrawGrid(const Grid<int32>& grid)
 void Main()
 {
 	Window::Resize(800, 800);
-	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
 
 	Grid<int32> grid(8, 8);
+	Grid<StoneColor> boardState(8, 8, StoneColor::None);
 
 	while (System::Update())
 	{
 		DrawGrid(grid);
+		DrawStone(boardState);
 	}
 }
